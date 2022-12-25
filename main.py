c06f9150a9da044923516faa85b967eeb170a3dd
YannQuinque <120184828+YannQuinque@users.noreply.github.com>
2022-12-25 11:44:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 ##################################
-#Déclaration des fonctions externes
+# Déclaration des fonctions externes
 
 import random
-from functions import*
+from fonctions import *
 
 #####################################
-#Corps du programme principal
+# Corps du programme principal
 
-if __name__=='__main__':
-    go=0
-    
-    print("Commencer a jouer (taper 1) \nAfficher les règles du jeu (taper 2)")
-    while go <1 and go <2:
-        go=int(input())
-    if go==2:
-        print("Tetris  vous met au défi de réaliser des lignes ou des collonnes complètes en déplaçant des pièces de formes différentes adapté au plateau choisi. Il vous suffit de saisir les coordonnées de l’endroit où vous voulez insérer le bloc. Les rangées  complétées disparaissent tout en rapportant des points et vous pouvez donc de nouveau remplir les cases libérées.")
-
+if __name__ == '__main__':
+    go = 0
+    while go < 1 or go > 2:
+        go =int(input("Commencer a jouer (Tapez 1) \nAfficher les règles du jeu (Tapez 2)"))
+    if go == 2:
+        print("Tetris  vous met au défi de réaliser des lignes ou des colonnes complètes en déplaçant des pièces de formes différentes adaptées au plateau choisi. \nIl vous suffit de saisir les coordonnées de l’endroit où vous voulez insérer le bloc.\nLes rangées  complétées disparaissent tout en rapportant des points et vous pouvez donc de nouveau remplir les cases libérées.")
     else:
-        grid=input("Saisissez une forme de plateau:\n1)cercle\n2)losange\n3)triangle\n4)Aléatoire")
+        Forme = 0
+        forme = 0
+        while Forme < 1 or Forme > 4:
+            Forme = int(input("Saisissez une forme de plateau:\nCercle(Tapez 1)\nLosange(Tapez 2)\nTriangle(Tapez 3)\nAléatoire(Tapez 4)\n"))
+            if Forme == 1:
+                forme = 'C'
+            elif Forme == 2:
+                forme = 'L'
+            elif Forme == 3:
+                forme = 'T'
+            elif Forme == 4:
+                forme = random.choice(['C','L','T'])
+                if forme == 'C':
+                    print('La forme choisie est le Carré')
+                elif forme == 'L':
+                    print('La forme choisie est le Losange')
+                elif forme == 'T':
+                    print('La forme choisie est le Triangle')
 
-    while grid<1 and grid>4:
-        if grid==1:
-            grid=losange
-        elif grid==2:
+        taille = 0
+        while taille < 21 or taille > 26:
+            taille =int(input("Saisissez la taille de votre grille de jeu (entre 21 et 26):"))
 
-        elif grid==3:
-
-        elif grid==4:
+        grille = read_grid(forme,taille)
+        save_grid('grille.txt',grille)
+        print_grid(grille)
